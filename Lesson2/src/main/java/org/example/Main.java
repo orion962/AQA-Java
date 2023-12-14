277e7eef5e22d2592a96f60699029ef52592dc29
orion962 <danilgalich111@gmail.com>
2023-12-14 21:07:44 +0300

    Check2

--- Lesson2/src/main/java/org/example/Main.java
+++ Lesson2/src/main/java/org/example/Main.java
@@ -1,19 +1,63 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        printThreeWords();
+        System.out.println();
+        checkSumSign();
+        System.out.println();
+        printColor();
+        System.out.println();
+        compareNumbers();
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    static void printThreeWords()
+    {
+        System.out.println("Orange\nBanana\nApple");
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    static void checkSumSign()
+    {
+        int a = 5;
+        int b = -9;
+        if (a + b >= 0)
+        {
+            System.out.println("Сумма положительная");
+        }
+        else
+        {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    static void printColor()
+    {
+        int value = 99;
+        if (value <= 0)
+        {
+            System.out.println("Красный");
+        }
+        else if (value > 0 && value <= 100)
+        {
+            System.out.println("Жёлтый");
+        }
+        else
+        {
+            System.out.println("Зелёный");
+        }
+    }
+
+    static void compareNumbers()
+    {
+        int a = 7;
+        int b = 12;
+        if (a >= b)
+        {
+            System.out.println("a >= b");
+        }
+        else
+        {
+            System.out.println("a < b");
         }
     }
 }
