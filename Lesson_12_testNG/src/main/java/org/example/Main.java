16946b6c83db073aa3f03537b79a4a51e24a595c
orion962 <danilgalich111@gmail.com>
2024-01-26 18:11:54 +0300

    com12(testNG)

---
+++ Lesson_12_testNG/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(calculateFactorial(10));
+    }
+
+    static int calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return calculateFactorial(n - 1) * n;
+        }
+    }
+}
---
+++ Lesson_12_testNG/src/test/java/org/example/MainTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MainTest {
+    @Test
+    public void testCalculateFactorial() {
+        assertEquals(1, Main.calculateFactorial(0));
+        assertEquals(1, Main.calculateFactorial(1));
+        assertEquals(120, Main.calculateFactorial(5));
+        assertEquals(3628800, Main.calculateFactorial(10));
+    }
+}
