f0f30e5079ab1c1ed8a3aa5466472ea0b89819dd
orion962 <danilgalich111@gmail.com>
2024-02-02 18:15:03 +0300

    com13

---
+++ Lesson13/src/test/java/TestClass.java
@@ -0,0 +1,69 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class TestClass {
+    private WebDriver driver;
+
+    @BeforeEach
+    @Deprecated
+    void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.get("https://www.mts.by/");
+        WebElement btn = driver.findElement(By.id("cookie-agree"));
+        btn.click();
+    }
+
+    @Test
+    @DisplayName("Проверка названия блока пополнения счёта")
+    void checkTitle() {
+        WebElement elem = driver.findElement(By.xpath("//div[@class='pay__wrapper']/h2"));
+        Assertions.assertEquals("Онлайн пополнение без комиссии", elem.getText().replace('\n', ' '));
+    }
+
+    @Test
+    @DisplayName("Проверка наличия логотипов платёжных систем")
+    void checkLogo() {
+        List<WebElement> elems = driver.findElements(By.xpath("//div[@class='pay__partners']//img"));
+        Assertions.assertTrue(elems.stream().allMatch(WebElement::isDisplayed));
+    }
+
+    @Test
+    @DisplayName("Проверка работы ссылки 'Подробнее о сервисе'")
+    void verifyLink() {
+        WebElement elem = driver.findElement(By.xpath("//*[text()='Подробнее о сервисе']"));
+        String linkUrl = elem.getAttribute("href");
+        try {
+            URL url = new URL(linkUrl);
+            HttpURLConnection httpURLConnect = (HttpURLConnection) url.openConnection();
+            httpURLConnect.connect();
+            Assertions.assertTrue(httpURLConnect.getResponseCode() < 400);
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    @DisplayName("Проверка работы кнопки 'Продолжить' после заполнения всех необходимых полей")
+    void checkButton() {
+        WebElement btn = driver.findElement(By.xpath("//div[@class='pay__wrapper']//button[text()='Продолжить']"));
+        WebElement phoneField = driver.findElement(By.xpath("//div[@class='pay__wrapper']//input[@class='phone']"));
+        WebElement moneyField = driver.findElement(By.xpath("//div[@class='pay__wrapper']//input[@class='total_rub']"));
+        phoneField.sendKeys("297777777");
+        moneyField.sendKeys("100");
+        btn.click();
+    }
+
+    @AfterEach
+    void finish() {
+        driver.quit();
+    }
+}
