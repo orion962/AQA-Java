2ad5094c41bb6b2d8edeea5838b2be5fa372cff8
orion962 <danilgalich111@gmail.com>
2024-02-16 15:56:47 +0300

    com14

--- Lesson14/src/test/java/TestClass.java
+++ Lesson14/src/test/java/TestClass.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -31,19 +33,16 @@ public class TestClass {
     void checkLabels() {
         WebElement btn = driver.findElement(By.xpath("//div[@class='pay__form']//button[@class='select__header']"));
         List<WebElement> list = driver.findElements(By.xpath("//div[@class='pay__form']//p[@class='select__option']"));
+        List<String> titles = new ArrayList<>(Arrays.asList("Номер телефона", "Номер абонента", "Номер счета на 44", "Номер счета на 2073"));
         Actions builder = new Actions(driver);
-        for (WebElement elem : list) {
+        for (int i = 0; i < list.size(); ++i) {
             builder.click(btn).build().perform();
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(
-                    By.xpath("//div[@class='pay__form']//ul[@class='select__list']")));
-            builder.pause(2000);
-            builder.click(elem).build().perform();
-            System.out.println(elem.getText() + ":");
-            for (WebElement input : driver.findElements(By.xpath("//form[@class='pay-form opened']//input"))) {
-                System.out.println(input.getAttribute("placeholder"));
-            }
-            System.out.println();
+            builder.pause(1000);
+            builder.click(list.get(i)).build().perform();
+            List<WebElement> input = driver.findElements(By.xpath("//form[@class='pay-form opened']//input"));
+            Assertions.assertEquals(titles.get(i), input.get(0).getAttribute("placeholder"));
+            Assertions.assertEquals("Сумма", input.get(1).getAttribute("placeholder"));
+            Assertions.assertEquals("E-mail для отправки чека", input.get(2).getAttribute("placeholder"));
         }
     }
 
@@ -53,6 +52,7 @@ public class TestClass {
         WebElement btn = driver.findElement(By.xpath("//div[@class='pay__wrapper']//button[text()='Продолжить']"));
         WebElement phoneField = driver.findElement(By.xpath("//div[@class='pay__wrapper']//input[@class='phone']"));
         WebElement moneyField = driver.findElement(By.xpath("//div[@class='pay__wrapper']//input[@class='total_rub']"));
+        List<String> titles = new ArrayList<>(Arrays.asList("Номер карты", "Срок действия", "CVC", "Имя держателя (как на карте)"));
         Actions builder = new Actions(driver);
         builder.sendKeys(phoneField, "297777777").build().perform();
         builder.sendKeys(moneyField, "100").build().perform();
@@ -66,19 +66,22 @@ public class TestClass {
         WebElement btnLb = driver.findElement(By.xpath("//button[contains(@class,'colored disabled')]"));
         List<WebElement> labels = driver.findElements(By.xpath(
                 "//div[@class='app-wrapper__content']//div[contains(@class, 'card ng-tns')]//label"));
-        System.out.println(sum.getText());
-        System.out.println(hd.getText());
-        System.out.println(btnLb.getText());
-        System.out.println();
-        for (WebElement label : labels) {
-            System.out.println(label.getText());
+        Assertions.assertTrue(sum.getText().contains("100"));
+        Assertions.assertTrue(btnLb.getText().contains("100"));
+        Assertions.assertTrue(hd.getText().contains("297777777"));
+        for (int i = 0; i < 4; ++i) {
+            Assertions.assertEquals(titles.get(i), labels.get(i).getText());
         }
-        Assertions.assertTrue(driver.findElement(
-                By.xpath("//div[@class='app-wrapper__content']//div[contains(@class, 'cards-brands')]")).isDisplayed());
+        List<WebElement> card_brands = driver.findElements(By.xpath("//div[@class='app-wrapper__content']//" +
+                "div[contains(@class, 'cards-brands')]//img"));
+        for (WebElement elem : card_brands) {
+            Assertions.assertTrue(elem.isDisplayed());
+        }
     }
 
     @AfterEach
     void finish() {
+        driver.close();
         driver.quit();
     }
 }
